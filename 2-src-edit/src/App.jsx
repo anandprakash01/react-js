55fb086da8b277db005067f9ff9e0f90ec698e8b
anandprakash01 <hr.anand0101@gmail.com>
2023-04-15 05:34:10 +0530

    changes

---
+++ 2-src-edit/src/App.jsx
@@ -0,0 +1,11 @@
+
+const App=()=>{
+    return (
+        <div>
+            <header>Header</header>
+            <main>Main</main>
+            <footer>Footer</footer>
+        </div>
+    );
+}
+export default App;
