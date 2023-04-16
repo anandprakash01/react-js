4161e6c321e681a41fa2662ec379567d2080e4b2
anandprakash01 <hr.anand0101@gmail.com>
2023-04-17 01:23:12 +0530

    changes

--- 3-componants/src/App.jsx
+++ 3-componants/src/App.jsx
@@ -1,10 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
-function App() {
-  return
+// Header Component
+const Header = () => {
+  return (
+    <>
+      <Button />
+      <header>This is header</header>
+    </>
+  );
+};
+
+// Nav Component
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Contact</li>
+        <li>About</li>
+        <li>FAQ</li>
+      </ul>
+    </nav>
+  );
 }
 
-export default App
+// Main Component
+const Main = () => {
+  return (
+    <main>
+      <article>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, quis!
+      </article>
+      <article>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, quis!
+      </article>
+    </main>
+  );
+};
+
+const Button = () => {
+  return <button>re-Usable button</button>;
+};
+
+// App
+function App() {
+  return (
+    <>
+      <Header />
+      <Button />
+      <Nav />
+      <Main />
+      <Button />
+    </>
+  );
+}
+
+export default App;
--- 3-componants/src/main.jsx
+++ 3-componants/src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 // import './index.css'
 
 // const container = document.querySelector("#root");
 // const root = createRoot(container);
 // root.render(<App />);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
